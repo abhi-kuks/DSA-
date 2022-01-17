ca37733e2d88372cdeca99fe62e9668777ae5a2b
Abhishek Kukreja <46806402+abhi-kuks@users.noreply.github.com>
2022-01-18 00:29:52 +0530

    Update sum_of_digits.py

--- Recursion/sum_of_digits.py
+++ Recursion/sum_of_digits.py
@@ -1,9 +1,9 @@
 def sum_of_digits(n):
   assert n>= 0 and int(n)==n, "Not applicable for negative and non integers"
-  if n in [0,1]:
-    return n
+  if n is 0:
+    return 0
   else:
-    return sum_of_digits(n//10) + n%10
+    return int(n%10) + sum_of_digits(int(n/10))
   
   
 print(sum_of_digits(18))
